95f50665c3217376fc29bd50585e6a211ec4946c
Matt Latham <matt.e.latham@gmail.com>
2016-05-30 16:55:26 -0400

    Further RLMArray support.

--- AFModel/Pod/ObjectProvider/AFPropertyInfo.m
+++ AFModel/Pod/ObjectProvider/AFPropertyInfo.m
@@ -1,7 +1,12 @@
 #import "AFPropertyInfo.h"
 
 
-#pragma mark Class Definition
+#pragma mark Class Variables
+
+static NSRegularExpression *_regularExpression;
+
+
+#pragma mark - Class Definition
 
 @implementation AFPropertyInfo
 {
@@ -14,6 +19,14 @@
 
 - (void)setPropertyType: (NSString *)propertyType
 {
+	static dispatch_once_t once;
+	dispatch_once(&once, ^
+	{
+		_regularExpression = [NSRegularExpression regularExpressionWithPattern: @"^RLMArray(<[a-z0-9]*>)$"
+			options: NSRegularExpressionCaseInsensitive
+			error: nil];
+	});
+
 	_propertyType = propertyType;
 	
 	// If this property is a pointer type, set the property class and class name (if available).
@@ -25,6 +38,16 @@
 		// Clear any quotes out of the class name.
 		typeName = [typeName stringByReplacingOccurrencesOfString: @"\""
 			withString: @""];
+		
+		NSTextCheckingResult *match = [_regularExpression firstMatchInString: typeName
+			options: 0
+			range: NSMakeRange(0, [typeName length])];
+		
+		if (match)
+		{
+			// Use a RLMArray directly.
+			typeName = @"RLMArray";
+		 }
 	
 		// Resolve and set the property class. NSClassFromString returns nil if typeName is not a class name.
 		_propertyClass = NSClassFromString(typeName);
